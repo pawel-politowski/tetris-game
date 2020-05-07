c5514fc19beb891f67b366a7467a7c11dbd24fcf
Paweł Politowski <matelly@poczta.onet.pl>
2020-05-07 21:35:06 +0200

    Almost done

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyledStartButton } from './styles/StyledStartButton'
+import { StyledStartButton } from './styles/StyledStartButton';
 
 const StartButton = ({ callback }) => (
-    <StyledStartButton onClick={callback}>START GAME</StyledStartButton>
+  <StyledStartButton onClick={callback}>Start Game</StyledStartButton>
 )
 
 export default StartButton;
